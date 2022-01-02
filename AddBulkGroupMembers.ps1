d82d6078618527c98c79526df303123dbcf21862
Abdullah Al ZMaili <50559046+abdullahzmaili@users.noreply.github.com>
2022-01-02 15:44:57 +0200

    Add files via upload

---
+++ AddBulkGroupMembers.ps1
@@ -0,0 +1,23 @@
+﻿## AddBulkGroupMesmbers
+$GroupID = Read-Host -Prompt "Please enter Group ID"
+$CSVPath = Read-Host -Prompt "Please enter CSV full path"
+
+# Connect to MSOnline Module
+Connect-MsolService
+
+# Import CSV sheet that contains the list Of Object IDs
+Import-CSv -Path $CSVPath | 
+ForEach { 
+    $UPN=$_.MemberId
+    
+    # Azure AD Group ID
+    add-MsolGroupMember -GroupObjectId $GroupID -GroupMemberObjectId $UPN -GroupMemberType User
+
+    Write-Host "$UPN has been added." -ForegroundColor Green
+}
+
+# To verify group memberships
+''
+''
+Write-Host "$GroupID memebers has been updated as the following:" -ForegroundColor Green
+Get-MsolGroupMember -GroupObjectId $GroupID
